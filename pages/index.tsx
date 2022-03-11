51a4434d97c7c5617b120947064ed1e9c2280f2e
LuisGH1234 <u201516808@upc.edu.pe>
2022-03-10 20:57:27 -0500

    cleaning unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const AppContainer = styled.div`
